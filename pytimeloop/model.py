ee79f217a0c0c0d6e63a286bf12b252809efa2de
Michael Gilbert <gilbertm@mit.edu>
2021-05-11 11:49:59 -0400

    Implement logging.

--- pytimeloop/model.py
+++ pytimeloop/model.py
@@ -2,25 +2,29 @@ from bindings import (NativeArchSpecs, NativeConfig,
                       NativeConfigNode, invoke_accelergy)
 from .config import Config
 
+import logging
+
 
 class ArchSpecs(NativeArchSpecs):
-    def __init__(self, config: Config, verbose=True):
+    def __init__(self, config: Config):
         _, native_arch_node = config.get_native()
         super().__init__(native_arch_node)
-        self.verbose = verbose
 
     def generate_tables(self, config: Config, semi_qualified_prefix, out_dir,
-                        out_prefix):
+                        out_prefix, log_level=logging.INFO):
+        # Setup logger
+        logger = logging.getLogger(__name__ + '.' + __class__.__name__)
+        logger.setLevel(log_level)
+
         native_root_cfg, native_cfg = config.get_native()
         root_node = native_root_cfg.get_root()
         if 'ERT' in root_node:
-            if self.verbose:
-                print('Found Accelergy ERT, replacing internal energy model')
+            logger.info('Found Accelergy ERT, replacing internal energy model')
             self.parse_accelergy_ert(root_node['ert'])
             if 'ART' in root_node:
-                if self.verbose:
-                    print('Found Accelergy ART, replacing internal area model')
-                self.parse_accelergy_art(root_ndoe['art'])
+                logger.info(
+                    'Found Accelergy ART, replacing internal area model')
+                self.parse_accelergy_art(root_node['art'])
         else:
             _, native_arch_cfg = config['architecture'].get_native()
             if 'subtree' in native_arch_cfg or 'local' in native_arch_cfg:
@@ -34,15 +38,13 @@ class ArchSpecs(NativeArchSpecs):
                 # garbage collected. CompoundConfigNode referes to it.
                 ert_cfg = NativeConfig(ert_path)
                 ert = ert_cfg.get_root().lookup('ERT')
-                if self.verbose:
-                    print('Generated Accelergy ERT to replace internal energy '
-                          'model')
+                logger.info('Generated Accelergy ERT to replace internal '
+                            'energy model')
                 self.parse_accelergy_ert(ert)
 
                 art_path = out_prefix + '.ART.yaml'
                 art_cfg = NativeConfig(art_path)
                 art = art_cfg.get_root()['ART']
-                if self.verbose:
-                    print('Generated Accelergy ART to replace internal energy '
-                          'model')
+                logger.info('Generated Accelergy ART to replace internal '
+                            'energy model')
                 self.parse_accelergy_art(art)
