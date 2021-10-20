d5c09a8aa025f469c6e87e5f3a47cb085dc4966e
Michael Gilbert <gilbertm@mit.edu>
2021-10-20 14:43:59 -0400

    Implement AcceleratorPool on Python side

--- pytimeloop/engine.py
+++ pytimeloop/engine.py
@@ -1,4 +1,4 @@
-from bindings import get_problem_shape, NativeEngine
+from bindings import get_problem_shape, NativeEngine, NativeAcceleratorPool
 from .model import ArchSpecs, SparseOptimizationInfo
 from .problem import Workload
 from .mapping import Mapping
@@ -37,9 +37,6 @@ class Accelerator(NativeEngine):
             AcceleratorEvalResult: result of accelerator evaluation. 
                 Has properties `eval_stat`, `utilization`, 
                 `total_energy`, `total_maccs`.
-
-        Todo:
-            Capture pretty print results somehow.
         """
         # Setup logger
         logger = logging.getLogger(__name__ + '.' + __class__.__name__)
@@ -47,11 +44,10 @@ class Accelerator(NativeEngine):
 
         # Perform pre-evaluation check and auto-bypass
         level_names = self.specs.level_names()
+        self.pre_eval_status = self.pre_evaluation_check(
+            mapping, workload, sparse_optimizations, False)
         if auto_bypass_on_failure:
-            pre_eval_status = self.pre_evaluation_check(
-                mapping, workload, sparse_optimizations, False)
-            self.pre_eval_status = pre_eval_status
-            for level, status in enumerate(pre_eval_status):
+            for level, status in enumerate(self.pre_eval_status):
                 if not status.success:
                     logger.warning("Couldn't map level ", level_names[level],
                                    ': ', self.pre_eval_status[level].fail_reason,
@@ -59,6 +55,9 @@ class Accelerator(NativeEngine):
             if not status.success:
                 for pvi in range(get_problem_shape().num_data_spaces):
                     mapping.datatype_bypass_nest[pvi].reset(level-1)
+        else:
+            return AcceleratorEvalResult(eval_status=None,
+                                         pre_eval_status=self.pre_eval_status)
 
         # Perform evaluation
         eval_status = super().evaluate(mapping, workload, sparse_optimizations)
@@ -67,7 +66,7 @@ class Accelerator(NativeEngine):
             if not status.success:
                 logger.error("Coulnd't map level ", level_names[level], ': ',
                              self.pre_eval_status[level].fail_reason)
-                return AcceleratorEvalResult(eval_status, pre_eval_status)
+                return AcceleratorEvalResult(eval_status, self.pre_eval_status)
 
         if self.is_evaluated():
             logger.info(
@@ -81,7 +80,7 @@ class Accelerator(NativeEngine):
         if return_stats:
             return self.get_stats(mapping)
         else:
-            return AcceleratorEvalResult(eval_status, pre_eval_status)
+            return AcceleratorEvalResult(eval_status, self.pre_eval_status)
 
     def get_stats(self, mapping):
         topology = self.get_topology()
@@ -91,12 +90,34 @@ class Accelerator(NativeEngine):
             topology.tile_sizes())
         return AcceleratorEvalResult(self.eval_status, self.pre_eval_status,
                                      self.utilization(), self.energy(),
-                                     self.area(),
+                                     self.area(), self.cycles(),
                                      topology.algorithmic_computes(),
                                      topology.actual_computes(),
+                                     topology.last_level_accesses(),
                                      topology.tile_sizes(),
                                      self.pretty_print_stats(),
                                      pretty_printed_mapping)
+
+
+class AcceleratorPool(NativeAcceleratorPool):
+    """A pool of accelerators."""
+    def __init__(self, specs: ArchSpecs, num_workers: int):
+        super().__init__(specs, num_workers)
+
+    def evaluate(self, mapping: Mapping, workload: Workload,
+                 sparse_opts: SparseOptimizationInfo, break_on_failure=False,
+                 auto_bypass_on_failure=True):
+        return super().evaluate(mapping, workload, sparse_opts,
+                                break_on_failure, auto_bypass_on_failure)
+
+    def get_result(self):
+        res = super().get_result()
+        return AcceleratorEvalResult(res.eval_status, res.pre_eval_status,
+                                     res.utilization, res.energy, res.area,
+                                     res.cycles, res.algorithmic_computes,
+                                     res.actual_computes,
+                                     res.last_level_accesses,
+                                     task_id=res.id)
 
 
 class AcceleratorEvalResult:
@@ -119,19 +140,23 @@ class AcceleratorEvalResult:
     """
 
     def __init__(self, eval_status, pre_eval_status, utilization=None,
-                 energy=None, area=None, algorithmic_compute=None,
-                 actual_compute=None, tile_sizes=None, pretty_printed_stats='',
-                 pretty_printed_mapping=''):
+                 energy=None, area=None, cycles=None, algorithmic_compute=None,
+                 actual_compute=None, last_level_accesses = None,
+                 tile_sizes=None, pretty_printed_stats='',
+                 pretty_printed_mapping='', task_id=None):
         self.eval_status = eval_status
         self.pre_eval_status = pre_eval_status
         self.utilization = utilization
         self.energy = energy
         self.area = area
+        self.cycles = cycles
         self.algorithmic_compute = algorithmic_compute
         self.actual_compute = actual_compute
+        self.last_level_accesses = last_level_accesses
         self.tile_sizes = tile_sizes
         self.pretty_printed_stats = pretty_printed_stats
         self.pretty_printed_mapping = pretty_printed_mapping
+        self.task_id = task_id
 
     def pretty_print_stats(self):
         return self.pretty_printed_stats
