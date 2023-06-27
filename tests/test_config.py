9e20833f7bf4134f532ca44696f8dcba75848d31
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-06-27 17:33:37 +0000

    implemented fuzz test case

--- tests/test_config.py
+++ tests/test_config.py
@@ -44,12 +44,14 @@ class CompoundConfigNodeTest(unittest.TestCase):
 
     ## @var The entropy library we're using.
     import random
-
     ## @var The seeded RNG we're using.
     rng: random.Random = random.Random(seed)
-
     ## @var The number of cycles we expect fuzz tests to perform.
     tests: int = 1000
+    ## @var The min value of the rng.
+    min_rng: int = 0
+    ## @var The max value of the rng.
+    max_rng: int = tests // 10
 
     ## @var The location of all our YAML files.
     root: str = (
@@ -58,6 +60,34 @@ class CompoundConfigNodeTest(unittest.TestCase):
         + "workspace/exercises/2020.ispass/timeloop"
     )
 
+    def check_node(self, truth: typing.Union[list, dict], node: ConfigNode):
+        """Checks that a node is equal to its equivalent truth. Returns nothing.
+        Values only arise from unittest printouts.
+
+        @param self     The test case module.
+        @param truth    The truth value we're referencing the Node against.
+        @param node     The node we're checking for correct reads.
+        """
+
+        # Defines different behavior is truth is a dict vs list.
+        truth_keys: list = None
+        # List truth.
+        if isinstance(truth, (list, tuple)):
+            truth_keys = range(len(truth))
+        # Dict truth.
+        else:
+            truth_keys = truth.keys()
+
+        # Goes through all the keys in truth.
+        key: typing.Union[int, str]
+        for key in truth_keys:
+            # If value is a scalar, compare.
+            if isinstance(truth[key], (bool, float, int, str, type(None))):
+                self.assertEqual(truth[key], node[key])
+            # Otherwise, it is a node, so recurse.
+            else:
+                self.check_node(truth[key], node[key])
+                    
     def test_accession(self) -> None:
         '''Tests getting from CompoundConfigNode when loaded by CompoundConfig.
 
@@ -91,33 +121,8 @@ class CompoundConfigNodeTest(unittest.TestCase):
 
                     # Pulls the Node (dict structure analog) from Config.
                     node: ConfigNode = compound_config.getRoot()
-
-                    def check_node(truth: typing.Union[list, dict], node: ConfigNode):
-                        """
-                        Checks that a node is equal to its equivalent truth.
-                        """
-
-                        # Defines different behavior is truth is a dict vs list.
-                        truth_keys: list = None
-                        # List truth.
-                        if isinstance(truth, (list, tuple)):
-                            truth_keys = range(len(truth))
-                        # Dict truth.
-                        else:
-                            truth_keys = truth.keys()
-
-                        # Goes through all the keys in truth.
-                        key: typing.Union[int, str]
-                        for key in truth_keys:
-                            # If value is a scalar, compare.
-                            if isinstance(truth[key], (bool, float, int, str, type(None))):
-                                self.assertEqual(truth[key], node[key])
-                            # Otherwise, it is a node, so recurse.
-                            else:
-                                check_node(truth[key], node[key])
-                    
                     # Starts recursive test.
-                    check_node(truth, node)
+                    self.check_node(truth, node)
                     
     def test_setting_fuzz(self) -> None:
         '''Tests setting to CompoundConfigNode when provided a random input.
@@ -134,11 +139,50 @@ class CompoundConfigNodeTest(unittest.TestCase):
 
         # Runs this amount of fuzz tests.
         for _ in range(self.tests):
-            pass
+            # Determines the key we want to insert to.
+            key: int = self.rng.randint(self.min_rng, self.max_rng)
+            # The rng value we wish to add.
+            val: float = self.rng.random()
+            
+            # Determines what type we want to insert.
+            match self.rng.randint(0, 4):
+                # Null type.
+                case 1:
+                    truth[key] = None
+                    root[key] = None
+                # Scalar, we will assume floats for simplicity.
+                case 2:
+                    truth[key] = val
+                    root[key] = val
+                # Sequence, we will append if list already at key.
+                case 3:
+                    # If not a list here, make a list here.
+                    if not key in truth or not isinstance(truth[key], list):
+                        truth[key] = []
+                    
+                    truth[key].append(val)
+                    root[key].append(val)
+                # Map, we will add to Map if Map already at key.
+                case 4:
+                    # If not a map here, make a map here.
+                    if not key in truth or not isinstance(truth[key], dict):
+                        truth[key] = {}
+                    
+                    # Generates the subkey.
+                    subkey: int = self.rng.randint(self.min_rng, self.max_rng / 100)
+
+                    # Does assignment to sub Map.
+                    truth[key][subkey] = val
+                    root[key][subkey] = val
+                # Undefined, not implemented but we might in the future.
+                case 0:
+                    pass
         
-
-
-
+            # Checks after every write.
+            self.check_node(truth, root)
+        
+        # And at the end, just in case.
+        self.check_node(truth, root)
 
 
 if __name__ == "__main__":
