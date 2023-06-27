cfe8d66a4f2c889beb54b9e2c66667fba35946a0
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-06-27 16:52:27 +0000

    uncommented other test, cannot find .util

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,26 +8,26 @@ import unittest
 import typing
 
 import bindings
-# from bindings.config import Configurator
+from bindings.config import Configurator
 from bindings.config import Config
 from bindings.config import ConfigNode
 
-# from .util import TEST_TMP_DIR, gather_yaml_configs
+from .util import TEST_TMP_DIR, gather_yaml_configs
 
 
-# class ConfigTest(unittest.TestCase):
-#     def test_config(self):
-#         CONFIG_DIR = Path('01-model-conv1d-2level')
-#         PATHS = ['arch/*.yaml',
-#                  'map/conv1d-2level-os.map.yaml',
-#                  'prob/*.yaml']
-#         yaml_str = gather_yaml_configs(CONFIG_DIR, PATHS)
-#         configurator = Configurator.from_yaml_str(yaml_str)
+class ConfigTest(unittest.TestCase):
+    def test_config(self):
+        CONFIG_DIR = Path('01-model-conv1d-2level')
+        PATHS = ['arch/*.yaml',
+                 'map/conv1d-2level-os.map.yaml',
+                 'prob/*.yaml']
+        yaml_str = gather_yaml_configs(CONFIG_DIR, PATHS)
+        configurator = Configurator.from_yaml_str(yaml_str)
 
-#         self.arch_specs = configurator.get_arch_specs()
-#         self.workload = configurator.get_workload()
-#         self.mapping = configurator.get_mapping()
-#         self.sparse_opts = configurator.get_sparse_opts()
+        self.arch_specs = configurator.get_arch_specs()
+        self.workload = configurator.get_workload()
+        self.mapping = configurator.get_mapping()
+        self.sparse_opts = configurator.get_sparse_opts()
 
 ## @var The testing seed.
 seed: int = 42
