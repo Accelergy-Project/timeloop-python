f2e8d8f9135c8e16332640fc0e60afd192ec4253
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-06-30 19:42:40 +0000

    binding boilerplate files

---
+++ bindings/buffer.cpp
@@ -0,0 +1,15 @@
+#include "pytimeloop/bindings/buffer.h"
+
+#include <optional>
+#include <variant>
+
+// PyBind11 headers
+#include "pybind11/stl.h"
+
+// Timeloop headers
+#include "model/buffer.hpp"
+
+namespace pytimeloop::buffer_bindings
+{
+    
+}
