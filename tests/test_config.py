508ac59e5b605ee6399eee01fdc38e3baae872f8
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-07-05 17:47:30 +0000

    cleaned up test_config a bit

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
-'''
+"""
 Does unit/integration testing for the configuration/inputs of PyTimeloop.
-'''
+"""
 
 import logging
 from pathlib import Path
@@ -23,7 +23,7 @@ from util import TEST_TMP_DIR, gather_yaml_configs
 
 class ConfigTest(unittest.TestCase):
     def run_evaluation(self, CONFIG_DIR: Path, PATHS: list[str]) -> Engine:
-        '''Creates and runs Timeloop given a configuration directory and paths
+        """Creates and runs Timeloop given a configuration directory and paths
         to the requisite YAML files.
 
         Outputs errors only through unittest asserts and print statements.
@@ -33,10 +33,10 @@ class ConfigTest(unittest.TestCase):
         @param PATHS        The paths of all the requisite files in the directory.
 
         @return             The engine after it finished evaluation.
-        '''
+        """
         # Combined YAML string of all the config files.
         yaml_str = gather_yaml_configs(CONFIG_DIR, PATHS)
-        
+
         # Loads the YAML into Configuration settings.
         config: Config = Config(yaml_str, "yaml")
         # Pulls out the Config root node, containing all the config info.
@@ -45,9 +45,10 @@ class ConfigTest(unittest.TestCase):
         # Creates the workload specified by root.
         workload: Workload = Workload(root["problem"])
         # Creates the architecture specified by root.
-        arch_specs: ArchSpecs = ArchSpecs(root["architecture"], 
-                                          "sparse_optimizations" in root)
-        
+        arch_specs: ArchSpecs = ArchSpecs(
+            root["architecture"], "sparse_optimizations" in root
+        )
+
         # Does accelergy load-ins if present.
         if "ERT" in root:
             arch_specs.parse_accelergy_ert(root["ERT"])
@@ -63,29 +64,27 @@ class ConfigTest(unittest.TestCase):
         engine: Engine = Engine(arch_specs)
         # Runs the evaluator.
         engine.evaluate(mapping, workload, sparse_info)
-        
+
         return engine
 
     def test_config_basic(self):
-        '''Tests that a loaded in Config can make a valid mapping.
+        """Tests that a loaded in Config can make a valid mapping.
 
         Basic test that Python can run a Timeloop configuration from start to
         finish off of pre-loaded files. Outputs are only through unittest asserts
         and print statements.
 
         @param self The testing unit environment.
-        '''
-        print("\n\n\nTesting Integration:\n" + '-'*5)
+        """
+        print("\n\n\nTesting Integration:\n" + "-" * 5)
         # Directory and path of all the config files.
-        CONFIG_DIR = Path('01-model-conv1d-2level')
-        PATHS = ['arch/*.yaml',
-                 'map/conv1d-2level-os.map.yaml',
-                 'prob/*.yaml']
+        CONFIG_DIR = Path("01-model-conv1d-2level")
+        PATHS = ["arch/*.yaml", "map/conv1d-2level-os.map.yaml", "prob/*.yaml"]
 
         print(self.run_evaluation(CONFIG_DIR, PATHS).pretty_print_stats())
 
     # def test_multiple_workloads(self):
-    #     '''Tests for any errors when there exist multiple Timeloop Workload 
+    #     '''Tests for any errors when there exist multiple Timeloop Workload
     #     instances.
 
     #     Errors are printed out through unittest asserts and print statements.
@@ -107,12 +106,13 @@ class ConfigTest(unittest.TestCase):
     #              'prob/*.yaml']
     #     self.run_evaluation(CONFIG_DIR, PATHS)
 
+
 ## @var The testing seed.
 seed: int = 42
 
+
 class CompoundConfigNodeTest(unittest.TestCase):
-    '''Tests the CompoundConfigNode class's reads and writes.
-    '''
+    """Tests the CompoundConfigNode class's reads and writes."""
 
     ## @var A parsed YAML file is used as the ground truth for our test cases.
     import yaml
@@ -122,6 +122,7 @@ class CompoundConfigNodeTest(unittest.TestCase):
 
     ## @var The entropy library we're using.
     import random
+
     ## @var The seeded RNG we're using.
     rng: random.Random = random.Random(seed)
     ## @var The number of cycles we expect fuzz tests to perform.
@@ -167,7 +168,7 @@ class CompoundConfigNodeTest(unittest.TestCase):
                 self.check_node(truth[key], node[key])
 
     def rummage_files(self, func: callable) -> None:
-        '''Goes through all files in the testing suite and runs a given test fxn
+        """Goes through all files in the testing suite and runs a given test fxn
         on the files, passing in the files as a string.
 
         All outputs are given through the unittest asserts and print statements.
@@ -175,47 +176,46 @@ class CompoundConfigNodeTest(unittest.TestCase):
         @param self The testing suite environment.
         @param func The testing function we want to do over all files in the
                     testing suite environment.
-        '''
+        """
         root: str
         _: list[str]
         files: list[str]
         # Goes through all the files in the testing directory.
         for root, _, files in self.os.walk(self.root, topdown=False):
-
             # Goes though all files.
             for file in files:
                 # Only runs test if we pulled in a YAML file.
                 if file.endswith(".yaml"):
                     # Constructs the file name.
                     filename: str = self.os.path.join(root, file)
-                    
+
                     # Open the file.
                     with open(filename, "r") as file:
                         # Reads the data as a string.
-                        data:str = file.read()
+                        data: str = file.read()
                         # Passes parsed data into testing fxn.
                         func(data)
 
     def test_accession(self) -> None:
-        '''Tests getting from CompoundConfigNode when loaded by CompoundConfig.
+        """Tests getting from CompoundConfigNode when loaded by CompoundConfig.
 
         Returns test results only through print statements generated through the
         unittest module's assert functions.
 
         @param self The unit test instance, so it can access test specific vars
                     like file locations.
-        '''
-        print("Testing Accessions:\n" + '-'*5)
+        """
+        print("Testing Accessions:\n" + "-" * 5)
 
         def accession_test(data: dict) -> None:
-            '''Given a string of a canonical YAML file, test that all accesses
+            """Given a string of a canonical YAML file, test that all accesses
             are possible.
 
             All errors are reported through print statements and the unittest
             assert functions.
 
             @param data The data in the canonical YAML file.
-            '''
+            """
             # Load the truth we're using for comparison.
             truth: dict = self.yaml.safe_load(data)
             # Load the truth into Config.
@@ -224,18 +224,18 @@ class CompoundConfigNodeTest(unittest.TestCase):
             node: ConfigNode = compound_config.getRoot()
             # Starts recursive test.
             self.check_node(truth, node)
-        
+
         self.rummage_files(accession_test)
-                    
+
     def test_setting_fuzz(self) -> None:
-        '''Tests setting to CompoundConfigNode when provided a random input.
+        """Tests setting to CompoundConfigNode when provided a random input.
 
         Returns test results only through print statements generated through the
         unittest module's assert functions.
 
         @param self The unit test instance this test belongs to.
-        '''
-        print("\n\n\nTesting Setters (Fuzz):\n" + '-'*5)
+        """
+        print("\n\n\nTesting Setters (Fuzz):\n" + "-" * 5)
 
         # Reference truth value.
         truth: dict = {}
@@ -248,7 +248,7 @@ class CompoundConfigNodeTest(unittest.TestCase):
             key: int = self.rng.randint(self.min_rng, self.max_rng)
             # The rng value we wish to add.
             val: float = self.rng.random()
-            
+
             # Determines what type we want to insert.
             yaml_type: int = self.rng.randint(0, 4)
             match yaml_type:
@@ -266,7 +266,7 @@ class CompoundConfigNodeTest(unittest.TestCase):
                     if (key not in truth) or not isinstance(truth[key], list):
                         truth[key] = []
                         root[key] = ConfigNode()
-                    
+
                     truth[key].append(val)
                     root[key].append(val)
                 # Map, we will add to Map if Map already at key.
@@ -275,7 +275,7 @@ class CompoundConfigNodeTest(unittest.TestCase):
                     if (key not in truth) or not isinstance(truth[key], dict):
                         truth[key] = {}
                         root[key] = ConfigNode()
-                    
+
                     # Generates the subkey.
                     subkey: int = self.rng.randint(self.min_rng, self.max_rng // 10)
 
@@ -285,48 +285,48 @@ class CompoundConfigNodeTest(unittest.TestCase):
                 # Undefined, not implemented but we might in the future.
                 case 0:
                     pass
-        
+
             # Checks after every write.
             self.check_node(truth, root)
-        
+
         # And at the end, just in case.
         self.check_node(truth, root)
 
     def test_replication(self) -> None:
-        '''Tests ability to write in a Config from scratch by duplicating existing
+        """Tests ability to write in a Config from scratch by duplicating existing
         Configs that are known goods.
 
         Error values are returned only through the unittest library's print
         statements/asserts.
 
         @param self The testing suite environment we're running in.
-        '''
-        print("\n\n\nTest Replication:\n" + '-'*5)
+        """
+        print("\n\n\nTest Replication:\n" + "-" * 5)
 
         def replication_test(file: str) -> None:
-            '''Given a canonical YAML string, test if the string can be written
+            """Given a canonical YAML string, test if the string can be written
             entirely in Python.
 
             Error values are returned only through the unittest library's print
             statements/asserts.
-            
+
             @param file The canonical YAML string.
-            '''
+            """
             # Loads in the reference value
             truth: dict = self.yaml.safe_load(file)
             # Creates the testing CompoundConfigNode
             root: ConfigNode = ConfigNode()
 
             def dupe_level(truth: typing.Union[dict, list], node: ConfigNode) -> None:
-                '''Deep copies this level of the tree. Recurses to copy the nested
+                """Deep copies this level of the tree. Recurses to copy the nested
                 levels.
 
                 We do not return anything; all copies go into node.
 
                 @param truth    The values we want to copy.
                 @param node     The place we want to copy the values to.
-                '''
-                match(truth):
+                """
+                match (truth):
                     # Dictionary copy.
                     case dict():
                         # Goes through all keys in truth.
@@ -361,7 +361,7 @@ class CompoundConfigNodeTest(unittest.TestCase):
 
                 # Checks equality at the end of duplicaiton.
                 self.check_node(truth, node)
-            
+
             dupe_level(truth, root)
             # Checks outside of recursion just in case.
             self.check_node(truth, root)
