026ed654c04934b00a109514fdcd03fa948c76d5
Michael Gilbert <gilbertm@mit.edu>
2024-05-30 13:42:09 -0400

    Bind and test Model and Mapper app

--- bindings/applications.cpp
+++ bindings/applications.cpp
@@ -1,6 +1,7 @@
 #include "pytimeloop/bindings/applications.h"
 
 #include <applications/mapper/mapper.hpp>
+#include <applications/mapper/mapper-thread.hpp>
 #include <applications/model/model.hpp>
 
 
@@ -8,26 +9,26 @@ namespace py = pybind11;
 
 namespace pytimeloop::application_bindings
 {
-  void BindModelApplication(py::module& m);
-  void BindMapperApplication(py::module& m);
-
   void BindApplications(py::module& m)
-  {
-    BindModelApplication(m);
-    BindMapperApplication(m);
-  }
-
-  void BindModelApplication(py::module& m)
   {
     py::class_<application::Model>(m, "ModelApp")
         .def(py::init<config::CompoundConfig*, std::string, std::string>())
         .def("run", &application::Model::Run);
-  }
 
-  void BindMapperApplication(py::module& m)
-  {
+    py::class_<application::Model::Stats>(m, "ModelResult")
+        .def_readwrite("cycles", &application::Model::Stats::cycles)
+        .def_readwrite("energy", &application::Model::Stats::energy);
+
     py::class_<application::Mapper>(m, "MapperApp")
         .def(py::init<config::CompoundConfig*, std::string, std::string>())
-        .def("run", &application::Mapper::Run);
+        .def("run", &application::Mapper::Run)
+        .def("get_global_best", &application::Mapper::GetGlobalBest);
+
+    // EvaluationResult in mapper-thread.cpp
+    py::class_<EvaluationResult>(m, "MapperResult")
+        .def_readwrite("valid", &EvaluationResult::valid)
+        .def_readwrite("mapping", &EvaluationResult::mapping)
+        .def_readwrite("stats", &EvaluationResult::stats);
+
   }
 }
