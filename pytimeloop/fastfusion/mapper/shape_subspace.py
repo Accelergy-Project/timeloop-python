f28f42b5c45aa0dde7dc75709f9c076d79f475a8
Michael Gilbert <gilbertm@mit.edu>
2024-10-21 16:31:12 -0400

    New TileShape iterator

--- pytimeloop/fastfusion/mapper/shape_subspace.py
+++ pytimeloop/fastfusion/mapper/shape_subspace.py
@@ -1,44 +1,41 @@
-from collections import defaultdict
-from collections.abc import Mapping, Sequence
-
 from combinatorics.integer import integer_factorizations_to_n_parts
 
 
 class ShapeSubspace:
-    def __init__(self,
-                 rank_shapes: Mapping[str, int],
-                 rank_order: Sequence[str]):
+    def __init__(self, rank_shapes: dict[int, int], ranks: list[int]):
         self.rank_shapes = rank_shapes
+        self.ranks = ranks
 
-        rank_idxs = defaultdict(list)
-        for i, r in enumerate(rank_order):
-            rank_idxs[r].append(i)
+        self.position_to_last = {}
+        self.fill_position_to_next()
 
-        self.permutation_map = []
-        self.choices = []
-        for rank, idxs in rank_idxs.items():
-            self.permutation_map += idxs
-            self.choices.append([
-                (shapes, rank, shapes[-1])
-                for shapes in get_shapes_of_int(rank_shapes[rank], len(idxs))
-            ])
+    def fill_position_to_next(self):
+        self.rank_to_last = {}
+        for i, r in enumerate(self.ranks):
+            if r in self.rank_to_last:
+                self.position_to_last[i] = self.rank_to_last[r]
+            else:
+                self.position_to_last[i] = None
+            self.rank_to_last[r] = i
 
     def __iter__(self) -> 'ShapeSubspaceIterator':
         return ShapeSubspaceIterator(self)
 
 
 class ShapeSubspaceIterator:
-    def __init__(self, subspace: ShapeSubspace):
-        self.subspace = subspace
-        self.permutation_map = subspace.permutation_map
-        self.choices = subspace.choices
+    def __init__(self, shape_subspace: ShapeSubspace):
+        self.shapes = shape_subspace.rank_shapes
+        self.ranks = shape_subspace.ranks
+        self.choice_generators = self.make_choice_generators(shape_subspace)
+        self.pos_to_last = shape_subspace.position_to_last
 
-        self.choice_iterators = [iter(choice) for choice in self.choices]
         self.is_started = False
         self.is_done = False
-        self.choice = None
-        self.is_first_choice = [False]*len(self.choices)
         self.just_skipped = False
+
+        self.choice_iterators = None
+        self.choice = None
+        self.is_first_choice = None
 
     def __iter__(self):
         return self
@@ -46,11 +43,7 @@ class ShapeSubspaceIterator:
     def __next__(self):
         if not self.is_started:
             self.is_started = True
-            try:
-                self.choice = [next(it) for it in self.choice_iterators]
-                self.is_first_choice = [True]*len(self.choice_iterators)
-            except StopIteration as e:
-                self.is_done = True
+            self.initialize_choice_iterators()
         elif not self.just_skipped:
             self.is_done = True
             for i in range(len(self.choice_iterators)):
@@ -59,14 +52,16 @@ class ShapeSubspaceIterator:
                     self.move_iterator(idx)
                     break
                 except StopIteration as e:
-                    self.restart_iterator(idx)
+                    pass
+            for j in range(idx+1, len(self.choice_iterators)):
+                self.restart_iterator(j)
         else:
             self.just_skipped = False
 
         if self.is_done:
             raise StopIteration()
 
-        return self.permute_choices_to_original()
+        return self.choice.copy()
 
     def skip_current_rank_iteration(self, chain_skip_if_first=True):
         if self.is_done:
@@ -85,10 +80,6 @@ class ShapeSubspaceIterator:
             self.just_skipped = True
             return
 
-        for i in range(skip_limit):
-            idx = len(self.choice_iterators)-i-1
-            self.restart_iterator(idx)
-
         self.is_done = True
         for i in range(skip_limit, len(self.choice_iterators)):
             idx = len(self.choice_iterators)-i-1
@@ -97,26 +88,43 @@ class ShapeSubspaceIterator:
                 break
             except StopIteration as e:
                 self.restart_iterator(idx)
+        for j in range(idx+1, len(self.choice_iterators)):
+            self.restart_iterator(j)
 
         self.just_skipped = True
 
-    def permute_choices_to_original(self):
-        reordered_choice = [None]*len(self.permutation_map)
+    def make_choice_generators(self, shape_subspace: ShapeSubspace):
+        choice_generators = []
+        for _ in shape_subspace.ranks:
+            choice_generators.append(
+                lambda shape: [
+                    s[0] for s in
+                    integer_factorizations_to_n_parts(shape, 2)
+                ]
+            )
+        return choice_generators
 
-        leftover_rank_shapes = self.subspace.rank_shapes.copy()
+    def initialize_choice_iterators(self):
+        self.choice_iterators = [None]*len(self.choice_generators)
+        self.choice = [None]*len(self.choice_generators)
+        self.is_first_choice = [None]*len(self.choice_generators)
+        for i in range(len(self.choice_generators)):
+            try:
+                self.restart_iterator(i)
+            except StopIteration as e:
+                self.is_done = True
+                return
 
-        appended_choice = []
-        for chosen_shape, rank, leftover_shape in self.choice:
-            appended_choice += chosen_shape
-            leftover_rank_shapes[rank] = leftover_shape
-
-        for i_prime, val in zip(self.permutation_map, appended_choice):
-            reordered_choice[i_prime] = val
-
-        return reordered_choice, leftover_rank_shapes
+            self.is_first_choice[i] = True
 
     def restart_iterator(self, idx):
-        self.choice_iterators[idx] = iter(self.choices[idx])
+        last = self.pos_to_last[idx]
+        if last is None:
+            shape = self.shapes[self.ranks[idx]]
+        else:
+            shape = self.choice[last]
+        self.choice_iterators[idx] = \
+            iter(self.choice_generators[idx](shape))
         self.choice[idx] = next(self.choice_iterators[idx])
         self.is_first_choice[idx] = True
 
@@ -125,14 +133,3 @@ class ShapeSubspaceIterator:
         self.choice[idx] = val
         self.is_first_choice[idx] = False
         self.is_done = False
-
-
-def get_shapes_of_int(n, levels):
-    factors = [tup[0] for tup in integer_factorizations_to_n_parts(n, 2)]
-    if levels == 1:
-        for f in factors:
-            yield [f]
-        return
-    for f in factors:
-        for shape in get_shapes_of_int(f, levels-1):
-            yield [f] + shape
---
+++ tests/fastfusion/test_shape_subspace.py
@@ -0,0 +1,41 @@
+import unittest
+
+from pytimeloop.fastfusion.mapper.shape_subspace import ShapeSubspace
+
+from tests.load_config_mixin import LoadConfigMixin
+
+
+class TestShapeSubspace(LoadConfigMixin, unittest.TestCase):
+    def setUp(self) -> None:
+        einsum_shape = {
+            0: 4,
+            1: 2,
+            2: 2
+        }
+        shape_subspace = ShapeSubspace(einsum_shape, [0, 1, 0, 2, 2])
+        self.subspace_it = iter(shape_subspace)
+
+    def test_iterate_all(self):
+        self.assertEqual(36, self.count_iterations(self.subspace_it))
+
+    def test_skip_first_iteration(self):
+        # Simulates first choice not being valid
+        first_choice = next(self.subspace_it)
+        self.subspace_it.skip_current_rank_iteration()
+        self.assertEqual(0, self.count_iterations(self.subspace_it))
+
+    def test_skip_second_rank_interation(self):
+        first_choice = next(self.subspace_it)
+        second_rank_val_in_first_choice = first_choice[2]
+        for shape in self.subspace_it:
+            if shape[2] != second_rank_val_in_first_choice:
+                break
+        self.subspace_it.skip_current_rank_iteration()
+        self.assertEqual(24, self.count_iterations(self.subspace_it))
+
+    @staticmethod
+    def count_iterations(it):
+        count = 0
+        for p in it:
+            count += 1
+        return count
