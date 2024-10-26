25059ac137f18c24148cb45d040775b685b9b5b0
tanner-andrulis <andrulis@mit.edu>
2024-10-26 09:06:32 -0400

    Fix tensor rank names

--- pytimeloop/fastfusion/playground.py
+++ pytimeloop/fastfusion/playground.py
@@ -12,19 +12,12 @@ from itertools import permutations
 
 
 def get_sims():
-    rank_sizes = {
-        "M1": 1024,
-        "K1": 1024,
-        "N1": 1024,
-        "N2": 1024,
-        "N3": 1024,
-        "N4": 1024,
-    }
+    RANK_SIZE = 512
 
     fusable_tensors = {
         "All"
     }  # {"A1", "C1", "C2", "C3", "C4"} | {"B1", "B2", "B3", "B4"}
-    # fusable_tensors = {"A1", "C1", "C2", "C3", "C4"}
+    fusable_tensors = {"A1", "C1", "C2", "C3", "C4", "C5", "C6", "C7"}
     must_fuse = set()  # set(["C1", "C2", "C3", "C4"])
 
     sims = []
@@ -69,10 +62,10 @@ def get_sims():
                     if not factors:
                         ms()
 
-                    while not all(f == rank_sizes[p] for f, p in zip(factors, perm)):
+                    while not all(f == RANK_SIZE for f, p in zip(factors, perm)):
                         for i in range(len(factors)):
                             factors[i] *= 2
-                            if factors[i] > rank_sizes[perm[i]]:
+                            if factors[i] > RANK_SIZE:
                                 factors[i] = 1
                             else:
                                 break
@@ -86,9 +79,9 @@ def get_sims():
         makesim("M2", {"C1": {"M1", "N1"}, "B2": {"N1", "N2"}, "C2": {"M1", "N2"}}),
         makesim("M3", {"C2": {"M1", "N2"}, "B3": {"N2", "N3"}, "C3": {"M1", "N3"}}),
         makesim("M4", {"C3": {"M1", "N3"}, "B4": {"N3", "N4"}, "C4": {"M1", "N4"}}),
-        makesim("M5", {"C4": {"M1", "N3"}, "B5": {"N3", "N4"}, "C5": {"M1", "N4"}}),
-        makesim("M6", {"C5": {"M1", "N3"}, "B6": {"N3", "N4"}, "C6": {"M1", "N4"}}),
-        makesim("M7", {"C6": {"M1", "N3"}, "B7": {"N3", "N4"}, "C7": {"M1", "N4"}}),
+        makesim("M5", {"C4": {"M1", "N4"}, "B5": {"N4", "N5"}, "C5": {"M1", "N5"}}),
+        makesim("M6", {"C5": {"M1", "N5"}, "B6": {"N5", "N6"}, "C6": {"M1", "N6"}}),
+        makesim("M7", {"C6": {"M1", "N6"}, "B7": {"N6", "N7"}, "C7": {"M1", "N7"}}),
     ]
     return sims
 
@@ -109,7 +102,8 @@ def run(sims: list[SIM]):
         ns = sims.pop(0)
 
         ns = SIM.group_by_left(ns, s[0].tensor_names)
-        [s2.consolidate(live_tensors) for s2 in s]
+        for s2 in s:
+            s2.consolidate(live_tensors)
         s = SIM.group_by_right(SIM.combine_combineable(s, live_tensors), live_tensors)
         for s2 in s.values():
             for s3 in s2:
@@ -118,14 +112,19 @@ def run(sims: list[SIM]):
         print("\n\n")
         print("\n\n" + "=" * 100 + f"\n{len(sims) + 1} Remaining\n" + "=" * 100)
 
+        DO_PRINT = False
+
         combined: list[SIM] = []
         for k in s:
             if k in ns:
                 for a, b in itertools.product(s[k], ns[k]):
-                    # print(f"\t{a.tiling_str()} <--> {b.tiling_str()}")
+                    if DO_PRINT:
+                        print(f"\t{a.tiling_str()} <--> {b.tiling_str()}")
                     combined.append(a.copy())
                     combined[-1].merge_next(b, set())
                     # combined_keys.append()
+            elif DO_PRINT:
+                print(f"\tNo match for {s[k][0].tiling_str()}")
 
         s = combined
         print(f"Generated {len(s)} solutions")
