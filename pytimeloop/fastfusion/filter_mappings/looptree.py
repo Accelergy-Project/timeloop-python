2abbe3b636db0b78748777f752420b200058c067
tanner-andrulis <andrulis@mit.edu>
2025-02-25 18:18:45 +0000

    Looptree bugfix

--- pytimeloop/fastfusion/filter_mappings/looptree.py
+++ pytimeloop/fastfusion/filter_mappings/looptree.py
@@ -8,17 +8,23 @@ def get_looptree_tag_mha(
     input_tensors: set[str],
     output_tensors: set[str],
     tiling: Tiling,
-    rank_name_to_shared_name: dict[str, str],
+    rank_name_to_shared_name: dict[str, str],   
     tensor_to_relevant_ranks,
 ):
+    fused_storages = [t for t in backing_storages if t.storage_name != 0]
     
-    is_fused = any(t.storage_name != 0 for t in backing_storages)
-    if not is_fused:
+    # Unfused
+    if not fused_storages:
         return ("LOOPTREE_VALID",)
-
-    n_loops_above_backing_storages = max([0] + [
-        t.above_loop_index for t in backing_storages if int(t.storage_name) != 0
-    ]
-    )
     
-    return ("LOOPTREE_VALID", f"FUSED_LOOPS={n_loops_above_backing_storages}")
+    # Fused with one side but not the other. We don't want to interfere with the
+    # unfused side, so just go LOOPTREE_VALID. The number of loops will be enforced
+    # by the tiling since it must match for the one fused tensor.
+    if len(fused_storages) == 1:
+        return ("LOOPTREE_VALID",)
+    
+    # Fused with both sides. Make sure that the number of loops is the same.
+    n_loops = set(t.above_loop_index for t in fused_storages)
+    if len(n_loops) > 1:
+        return ("LOOPTREE_INVALID",)
+    return ("LOOPTREE_VALID",)
